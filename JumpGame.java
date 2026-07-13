f07b2b1e68d3fbd31c77d03cff102836b0973a23
shahid003 <shahidkhandk1122@gmail.com>
2026-07-13 10:03:02 +0530

    jump Game

---
+++ JumpGame.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class JumpGame {
+
+    public static int jump(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return 0;
+        }
+
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
+                if (currentEnd >= nums.length - 1) {
+                    break;
+                }
+            }
+        }
+
+        return jumps;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        System.out.println("Enter the array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int result = jump(nums);
+        System.out.println("Minimum jumps needed = " + result);
+
+        sc.close();
+    }
+}
